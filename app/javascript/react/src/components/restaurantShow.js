1b12b31b093690ea66a0ca732bee848df3bb19c0
User <max@Users-MacBook-Air.local>
2017-11-06 14:36:26 -0500

    import typo

--- app/javascript/react/src/components/restaurantShow.js
+++ app/javascript/react/src/components/restaurantShow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import RestaurantShowSubmenu from './RestaurantShowSubmenu'
+import RestaurantShowSubmenu from './restaurantShowSubmenu'
 // import $ from 'jquery'
 
 

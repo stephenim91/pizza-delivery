2844c82fbd972e037b2185087ff2f51de45a2f3b
User <max@Users-MacBook-Air.local>
2017-11-08 14:37:18 -0500

    imported modals

--- app/javascript/react/src/components/restaurantShow.js
+++ app/javascript/react/src/components/restaurantShow.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 import RestaurantShowSubmenu from './restaurantShowSubmenu'
 // import $ from 'jquery'
+import Modal from 'react-modal';
 
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 
 
@@ -16,8 +28,13 @@ class RestaurantShow extends Component {
       deliveryMin: '',
       logoUrl: '',
       restaurantName: '',
-      restaurantApiKey: ''
+      restaurantApiKey: '',
+      modalIsOpen: false
     }
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+
   }
 
   componentDidMount() {
@@ -36,6 +53,20 @@ class RestaurantShow extends Component {
       this.setState({ taxRate: restaurant.taxRate, hours: restaurant.hours, deliveryPrice: restaurant.deliveryPrice, deliveryMin: restaurant.deliveryMin, logoUrl: restaurant.logoUrl, restaurantApiKey: restaurant.apiKey, restaurantName: restaurant.name })
     })
   }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
 
   render() {
     let products = this.state.menu.map(submenu => {
@@ -70,6 +101,30 @@ class RestaurantShow extends Component {
             <div className="row">
               <h3 className="show-page-header">{this.state.restaurantName}</h3>
             </div>
+
+            <div>
+              <button onClick={this.openModal}>Open Modal</button>
+              <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+              >
+
+                <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                <button onClick={this.closeModal}>close</button>
+                <div>I am a modal</div>
+                <form>
+                  <input />
+                  <button>tab navigation</button>
+                  <button>stays</button>
+                  <button>inside</button>
+                  <button>the modal</button>
+                </form>
+              </Modal>
+            </div>
+
             <div className="row">
               <div className="small-6 column">
                 <img src={this.state.logoUrl}></img>
